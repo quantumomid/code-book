620667578fff8b01994a390d0d4b75fa0cb94b84
quantumomid <omid.wakili@outlook.com>
2021-12-25 12:59:53 +0000

    set up esbuild wasm and do quick check

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as esbuild from "esbuild-wasm";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [ input, setInput ] = useState("");
+    const [ code, setCode ] = useState("");
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const startService = async () => {
+        // initialise esbuid to fetch the bundle from public folder
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: "/esbuild.wasm"
+        });
+        console.log(service);
+    };
+
+    const handleClick = () => {
+        console.log(input);
+    };
+
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={handleClick}>Submit</button>
+            <pre>{ code }</pre>
+        </div>
+    );
+};
 
 export default App;
