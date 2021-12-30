0ed7052124f6b5d83a445b97d00ac7ca314548f5
quantumomid <omid.wakili@outlook.com>
2021-12-30 12:03:03 +0000

    add more config options for monaco editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,9 +1,21 @@
 import MonacoEditor from "@monaco-editor/react";
 
-const CodeEditor = () => {
-    return (
-        <MonacoEditor language="javascript" height="40vh"/>
-    )
-}
+const CodeEditor = () => (
+    <MonacoEditor 
+        theme="dark" 
+        language="javascript" 
+        height="40vh"
+        options={{
+            wordWrap: "on",
+            minimap: { enabled: false },
+            showUnused: false,
+            fontSize: 16,
+            scrollBeyondLastLine: false,
+            folding: false,
+            lineNumbersMinChars: 3,
+            automaticLayout: true,
+        }}
+    />
+)
 
 export default CodeEditor;
