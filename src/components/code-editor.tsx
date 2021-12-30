e85bf368f53b5e8f3f823d506a2a42cbaab9042f
quantumomid <omid.wakili@outlook.com>
2021-12-30 13:08:08 +0000

    add prettier formatting and set up and configure on monaco react editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
     initialValue: string;
@@ -7,11 +9,14 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-    
+    // Below reference is for handleFormat to have access to the monaco editor
+    const editorRef = useRef<any>();
+
     // editorDidMount ONLY runs when editor first displayed on screen
     // First parameter is a callback that gives the current value
     // Second parameter is a reference to actual editor - therefore use this to keep track of changes made
     const handleEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+        editorRef.current = monacoEditor; // Just to give handleFormat access to monaco editor
         // console.log(getValue());
         monacoEditor.onDidChangeModelContent(() => {
             // console.log(getValue());
@@ -21,24 +26,45 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
     };
 
+    const handleFormat = () => {
+        // Get current value from editor
+        const currentUnformattedValue = editorRef.current.getModel().getValue();
+        console.log(currentUnformattedValue);
+
+        // Format the value
+        const formattedValue = prettier.format(currentUnformattedValue, {
+            parser: "babel",
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: false,
+        });
+
+        // Set the formatted value back into the editor!
+        editorRef.current.setValue(formattedValue);
+    };
+
     return (
-        <MonacoEditor
-            value={initialValue}
-            editorDidMount={handleEditorDidMount}
-            theme="dark" 
-            language="javascript" 
-            height="40vh"
-            options={{
-                wordWrap: "on",
-                minimap: { enabled: false },
-                showUnused: false,
-                fontSize: 16,
-                scrollBeyondLastLine: false,
-                folding: false,
-                lineNumbersMinChars: 3,
-                automaticLayout: true,
-            }}
-        />
+        <div>
+            <button onClick={handleFormat}>Format</button>
+            <MonacoEditor
+                value={initialValue}
+                editorDidMount={handleEditorDidMount}
+                theme="dark" 
+                language="javascript" 
+                height="40vh"
+                options={{
+                    wordWrap: "on",
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    folding: false,
+                    lineNumbersMinChars: 3,
+                    automaticLayout: true,
+                }}
+            />
+        </div>
     )
 }
 
