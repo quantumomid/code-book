e33bf4fe7a2cf1133ad479178c0c15ea9859245f
quantumomid <omid.wakili@outlook.com>
2022-01-05 13:22:43 +0000

    accounting for file name variations in possible user commands

--- codebook/packages/cli/dist/commands/serve.js
+++ codebook/packages/cli/dist/commands/serve.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
+const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
 const local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
@@ -9,5 +13,8 @@ exports.serveCommand = new commander_1.Command()
     .option("-p, --port <number>", "Port to run server on", "4007")
     .action((filename = "notebook.js", options) => {
     // console.log({ filename, options });
-    (0, local_api_1.serve)(parseInt(options.port), filename, "/");
+    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    // console.log("ABSOLUTE PATH:", dir);
+    // console.log("Extracted FILENAME", path.basename(filename));
+    (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
 });
