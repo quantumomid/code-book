f97285a32224ed740d1026e16d76f98bd629931b
quantumomid <omid.wakili@outlook.com>
2022-01-05 14:07:10 +0000

    basic set up for express server for the local api

--- codebook/packages/local-api/dist/index.js
+++ codebook/packages/local-api/dist/index.js
@@ -1,11 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const express_1 = __importDefault(require("express"));
 const serve = (port, filename, dir) => {
-    console.log({
-        serving_traffic_on_port: port,
-        saving_cells_from: filename,
-        that_file_is_in_dir: dir
+    // console.log({
+    //     serving_traffic_on_port: port,
+    //     saving_cells_from: filename,
+    //     that_file_is_in_dir: dir
+    // });
+    const app = (0, express_1.default)();
+    app.listen(port, () => {
+        console.log("Listening on port: ", port);
     });
 };
 exports.serve = serve;
--- codebook/packages/local-api/src/index.ts
+++ codebook/packages/local-api/src/index.ts
@@ -1,7 +1,14 @@
+import express from "express";
+
 export const serve = (port:number, filename:string, dir:string) => {
-    console.log({
-        serving_traffic_on_port: port,
-        saving_cells_from: filename,
-        that_file_is_in_dir: dir
+    // console.log({
+    //     serving_traffic_on_port: port,
+    //     saving_cells_from: filename,
+    //     that_file_is_in_dir: dir
+    // });
+
+    const app = express();
+    app.listen(port, () => {
+        console.log("Listening on port: ", port);
     });
 };
